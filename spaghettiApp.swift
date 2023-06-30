4a883b54c1dc2710388f07b874331599a4afbdf9
ehiem <137926522+ehiem@users.noreply.github.com>
2023-06-30 12:58:09 -0600

    Add files via upload

---
+++ spaghettiApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaghettiApp.swift
+//  spaghetti
+//
+//  Created by Scholar on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct spaghettiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
